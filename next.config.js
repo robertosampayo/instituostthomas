c9a3d07151e5cec99a37237209cf06851ad48aaf
robertosampayo <jrobertosampayo@gmail.com>
2020-05-18 16:24:14 -0300

    Changes into api mail

--- next.config.js
+++ next.config.js
@@ -5,7 +5,7 @@ module.exports = {
   env: {
     SENDGRID_API_KEY: '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',
     // REACT_APP_API: 'http://localhost:8080/sitios/instituostthomas/api/contact/index.php',
-    REACT_APP_API: 'https://institutodeinglesstthomas.com/api/contact/index.php',
+    REACT_APP_API: 'https://ambientedetest.com//api/contact/index.php',
 
   },
 }
